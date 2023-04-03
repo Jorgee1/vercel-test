795fa7e789978af13c00e30b4019428d371af562
Jorgee1 <21368470+Jorgee1@users.noreply.github.com>
2023-04-03 09:17:40 -0500

    Vercel seems to crash if accesing /api/trpc. Moved outside

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
-import { appRouter } from './trpc'
+import { appRouter } from '../trpc'
 
 export const config = {
     runtime: 'edge'
